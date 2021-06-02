55da95c276645be9afa0587813e2cd4319c076d4
Frédéric Warren <derfwarren@hotmail.com>
2021-06-02 16:06:50 -0400

    Change mentions of Shotgun to ShotGrid

--- after_project_create.py
+++ after_project_create.py
@@ -10,14 +10,14 @@
 
 """
 The after_project_create file is executed as part of creating a new project.
-If your starter config needs to create any data in shotgun or do any other
+If your starter config needs to create any data in ShotGrid or do any other
 special configuration, you can add it to this file.
 
 The create() method will be executed as part of the setup and is passed
 the following keyword arguments:
 
-* sg -         A shotgun connection
-* project_id - The shotgun project id that is being setup
+* sg -         A ShotGrid connection
+* project_id - The ShotGrid project id that is being setup
 * log -        A logger instance to which progress can be reported via
                standard logger methods (info, warning, error etc)
 
